4a8b041e83d48011795861cf110d8e654550ffe4
Daniel Ortiz <jdoavila@gmail.com>
2014-04-13 22:08:28 -0600

    correos

--- webapp/views.py
+++ webapp/views.py
@@ -679,7 +679,7 @@ def APIsendvalidarcorreo(request):
 	contenido = 'Welcome to PartyOn\n'
 	contenido += 'You need to confirm your email address in order to enjoy all the features of PartyOn, click on the link below to verify your email.\n'
 	contenido += 'http://www.partyonapp.com/API/verify/email/?userProfcodeActivationclass=23das22das22d25&requestuidverifyEmail=' + str(usuario.UserProfileID) + '&markvalid=true'
-	correo = EmailMessage(titulo, contenido, to=[str(usuario2.email)])
+	correo = EmailMessage(titulo, contenido, from_email='no_reply@partyonapp.com', to=[str(usuario2.email)])
 	correo.send()
 
 	return HttpResponse("<h1>Se ha enviado tu email | PartyOn</h1>")
